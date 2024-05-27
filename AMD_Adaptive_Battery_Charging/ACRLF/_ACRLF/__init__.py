1c426f431714199450adf0c1c83e5c4d82424c0e
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2024-05-27 11:58:45 -0500

    Update __init__.py

--- AMD_Adaptive_Battery_Charging/ACRLF/_ACRLF/__init__.py
+++ AMD_Adaptive_Battery_Charging/ACRLF/_ACRLF/__init__.py
@@ -1,8 +1,5 @@
 # __init__.py
 # registers the environments with gym
-# 
-# Copyright (c) 2023 Advanced Micro Devices Inc.
-# Report Bugs to Shamith Achanta <Shamith.Achanta@amd.com>
 #
 
 import gymnasium as gym
