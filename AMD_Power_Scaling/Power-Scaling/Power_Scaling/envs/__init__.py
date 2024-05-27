0f58a358a9c281f3bc4e9bfc0e1a734f7c8d4965
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2024-05-27 12:06:15 -0500

    Update __init__.py

--- AMD_Power_Scaling/Power-Scaling/Power_Scaling/envs/__init__.py
+++ AMD_Power_Scaling/Power-Scaling/Power_Scaling/envs/__init__.py
@@ -1,8 +1,5 @@
 # __init__.py
 # imports required environment
-# 
-# Copyright (c) 2022 Shamith Achanta
-# Report Bugs to Shamith Achanta <shamith2@illinois.edu>
 #
 
 from Power_Scaling.envs.PowerScaling_env import PowerScaling
