0a601eed351a647280e71397111c7fa5b9dee2ef
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2024-05-27 11:58:59 -0500

    Update __init__.py

--- AMD_Adaptive_Battery_Charging/ACRLF/_ACRLF/envs/__init__.py
+++ AMD_Adaptive_Battery_Charging/ACRLF/_ACRLF/envs/__init__.py
@@ -1,8 +1,5 @@
 # __init__.py
 # imports required environment
-# 
-# Copyright (c) 2023 Advanced Micro Devices Inc.
-# Report Bugs to Shamith Achanta <Shamith.Achanta@amd.com>
 #
 
 from _ACRLF.envs.ACRLF import ACRLF
