98e98b27965a5ed5bc972b98f03ce0f0e94afecb
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2024-05-27 11:58:28 -0500

    Update setup.py

--- AMD_Adaptive_Battery_Charging/ACRLF-gym/setup.py
+++ AMD_Adaptive_Battery_Charging/ACRLF-gym/setup.py
@@ -8,6 +8,6 @@ from setuptools import setup
 setup(name="ACRLF",
     version='0.0.1',
     author="Shamith Achanta",
-    author_email="Shamith.Achanta@amd.com",
+    author_email="achantashamith007@gmail.com",
     install_requires=["gym==0.17.2", "numpy>=1.18.0", "psutil", "pandas", "scikit-learn", "jdatetime", "wmi"]
 )
