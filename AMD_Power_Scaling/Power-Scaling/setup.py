6a4d00df591ce7069568995fd5eba25dea73f9b0
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2024-05-27 12:05:32 -0500

    Update setup.py

--- AMD_Power_Scaling/Power-Scaling/setup.py
+++ AMD_Power_Scaling/Power-Scaling/setup.py
@@ -1,9 +1,6 @@
 # setup.py
 # downloads dependency packages for the required environment
 # add required packages in install_requires
-# 
-# Copyright (c) 2022 Shamith Achanta
-# Report Bugs to Shamith Achanta <shamith2@illinois.edu>
 #
 
 from setuptools import setup
@@ -11,6 +8,6 @@ from setuptools import setup
 setup(name="Power_Scaling",
     version='0.0.1',
     author="Shamith Achanta",
-    author_email="shamith2@illinois.edu",
+    author_email="achantashamith007@gmail.com",
     install_requires=["gym==0.21.0", "numpy>=1.18.0"]
 )
